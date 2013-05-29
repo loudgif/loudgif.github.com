662681be241e6bdf83912f9fb50cc992de2ddef8
Garrett Miller <heyitsgarrett@gmail.com>
2013-05-29 00:00:01 -0400

    IT BEGINS

---
+++ js/yt.js
@@ -0,0 +1,49 @@
+// Youtube Embed Code
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    videoId: 's7L2PVdrb_8',
+    playerVars: { 'autoplay': 1, 'controls': 0 },
+    events: {
+      'onReady': onPlayerReady,
+      'onPlaybackQualityChange': onPlayerPlaybackQualityChange,
+      'onStateChange': onPlayerStateChange,
+      'onError': onPlayerError
+    }
+  });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function onPlayerPlaybackQualityChange(event){
+
+}
+
+function onPlayerError(event) {
+
+}
+
+// var done = false;
+function onPlayerStateChange(event) {
+// if (event.data == YT.PlayerState.PLAYING && !done) {
+//   setTimeout(stopVideo, 6000);
+//   done = true;
+// }
+}
+
+// function stopVideo() {
+//   player.stopVideo();
+// }
